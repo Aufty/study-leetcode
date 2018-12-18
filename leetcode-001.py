54dd71cc41095d5ad9d6f4452f6e74689136d397
Jacob Aufderheide <jake-mac@JakeMBP.local>
2018-12-18 07:45:09 -0800

    Finished other potential solutions for problem 1

--- leetcode-001.py
+++ leetcode-001.py
@@ -1,5 +1,4 @@
 # Brute force slow solution
-
 class Solution1:
     def twoSum(self, nums, target):
         """
@@ -7,14 +6,36 @@ class Solution1:
         :type target: int
         :rtype: List[int]
         """
-        for x in nums:
+        for x in range(0, len(nums)):
             for y in range(x+1, len(nums)):
                 if (nums[x]+nums[y] == target):
                     return [x,y]
         raise ValueError('No answer')
 
-sol = Solution1()
+# Solution 2
+# Loops through twice
+class Solution2:
+    def twoSum(self, nums, target):
+        valuesSeen = {}
+        for x in range(0, len(nums)):
+            valuesSeen[nums[x]] = x
+        for x in range(0, len(nums)):
+            compliment = target - nums[x]
+            if compliment in valuesSeen:
+                return [x, valuesSeen[compliment]]
+
+# Solution 3
+# Loops through once
+# Submitted Solution
+class Solution3:
+    def twoSum(self, nums, target):
+        valuesSeen = {}
+        for x in range (0, len(nums)):
+            compliment = target - nums[x]
+            if compliment in valuesSeen:
+                return [valuesSeen[compliment], x]
+            valuesSeen[nums[x]] = x
+
+sol = Solution3()
 answer = sol.twoSum([2,7,12,23], 9)
 print(answer)
-
-
